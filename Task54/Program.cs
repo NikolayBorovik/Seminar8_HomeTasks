2f2e541faa08e0dabcedc84e81641cdf6478a819
Nikolay Borovik <nik.Borovik@bk.ru>
2023-03-01 23:00:39 +1000

    initial commit

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива. // Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+int[,] GetIntMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SortToDescend(int[,] matrix)
+{
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - i - 1; j++)
+            {
+                if (matrix[k, j] < matrix[k, j + 1])
+                {
+                    int temp = matrix[k, j + 1];
+                    matrix[k, j + 1] = matrix[k, j];
+                    matrix[k, j] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = GetIntMatrix(4, 4);
+
+PrintMatrix(matrix);
+
+SortToDescend(matrix);
+
+System.Console.WriteLine();
+
+PrintMatrix(matrix);
